6c9e8d390af5d15b60f8f874f5c74a1eed1a1695
raycre8-g <raycre8tives@gmail.com>
2025-02-06 20:45:23 +0200

    Dukascopy Update

--- build/lib/dukascopy/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from .fetch import fetch_day
-from .csv_dumper import CSVDumper
-from .utils import valid_date, Logger, set_up_signals
-from .processor import decompress
-
-__all__ = ['fetch_day', 'decompress', 'CSVDumper', 'valid_date', 'Logger', 'set_up_signals']
-
-
